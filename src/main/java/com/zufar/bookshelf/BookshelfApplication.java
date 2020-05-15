c9c14c45af1e0ae9f6887f6f5981b0627dbe91de
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-05-15 22:46:23 +0400

    Initial commit

---
+++ src/main/java/com/zufar/bookshelf/BookshelfApplication.java
@@ -0,0 +1,12 @@
+package com.zufar.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zufar/bookshelf/BookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zufar.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
