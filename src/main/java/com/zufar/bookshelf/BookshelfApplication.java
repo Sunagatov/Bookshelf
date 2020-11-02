4359d2fe051354beec92004b6607b5535e12c8f4
Zufar_Sunagatov <zufar.sunagatov@gmail.com>
2020-11-03 00:01:31 +0300

    EnableJpaAuditing was added.

--- src/main/java/com/zufar/bookshelf/BookshelfApplication.java
+++ src/main/java/com/zufar/bookshelf/BookshelfApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties
 @SpringBootApplication
